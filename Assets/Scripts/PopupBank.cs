3db5059a6d8d2f17011bfa0d278ce5a0aa2ea5de
Sangyeop-Lim <dladla5675@gmail.com>
2025-06-05 15:10:46 +0900

    feat : 입출금 창 이동 구현
    입출금 창 이동 구현

---
+++ Assets/Scripts/PopupBank.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupBank : MonoBehaviour
+{
+    [SerializeField] private GameObject atmPanel;
+    [SerializeField] private GameObject depositPanel;
+    [SerializeField] private GameObject withdrawalPanel;
+
+    public void OnClickDeposit()
+    {
+        atmPanel.SetActive(false);
+        depositPanel.SetActive(true);
+    }
+
+    public void OnClickWithdrawal()
+    {
+        atmPanel.SetActive(false);
+        withdrawalPanel.SetActive(true);
+    }
+
+    public void OnClickBackFromDeposit()
+    {
+        depositPanel.SetActive(false);
+        atmPanel.SetActive(true);
+    }
+
+    public void OnClickBackFromWithdrawal()
+    {
+        withdrawalPanel.SetActive(false);
+        atmPanel.SetActive(true);
+    }
+}
